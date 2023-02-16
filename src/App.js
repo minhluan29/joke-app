683bcc23de91d90d838a52ba6c7927bcabbfe510
minhluan29 <minhluann291@gmail.com>
2023-02-16 17:27:15 +0700

    Update
    update image
    upfate css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast, Toaster } from "react-hot-toast";
 import "./App.css";
+import { images } from "./data";
 
 function App() {
   const [state, setstate] = useState([]);
@@ -49,7 +50,7 @@ function App() {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className="text-center">
       {/* header */}
       <div
         style={{
@@ -58,28 +59,55 @@ function App() {
           padding: "0px 200px",
         }}
       >
-        <p style={{ fontSize: "35px", fontWeight: "700" }}>LOGO</p>
+        <img
+          style={{ width: "100px", height: "100px" }}
+          src={images.Logo}
+          alt="Logo"
+        />
         <div
           style={{
             display: "flex",
             justifyContent: "center",
           }}
         >
-          <div>
-            <p>Handicrafted by</p>
-            <p>Jim HLS</p>
+          <div style={{ paddingTop: "25px", textAlign: "right" }}>
+            <p
+              style={{
+                color: "gray",
+                fontFamily: "sans-serif",
+                fontSize: "13px",
+                margin: "0px",
+              }}
+            >
+              Handicrafted by
+            </p>
+            <p style={{ fontSize: "14px", margin: "0px" }}>Jim HLS</p>
           </div>
+          <img
+            style={{ width: "85px", height: "85px" }}
+            src={images.Avatar}
+            alt="avatar"
+          />
         </div>
       </div>
 
       {/* Nav */}
       <div
-        style={{ backgroundColor: "green", padding: "50px 0", color: "white" }}
+        style={{
+          backgroundColor: "#29b363",
+          padding: "50px 0",
+          color: "white",
+        }}
       >
-        <p style={{ fontSize: "30px", fontWeight: "700" }}>
+        <p
+          style={{
+            fontSize: "30px",
+            fontWeight: "500",
+          }}
+        >
           A joke a day keeps the doctor away
         </p>
-        <p style={{ fontSize: "14px" }}>
+        <p style={{ fontSize: "12px", fontWeight: "500" }}>
           If you joke wrong way, your teeth have to pay. (Serious)
         </p>
       </div>
@@ -87,42 +115,32 @@ function App() {
       {/* Main */}
       <div>
         <div>
-          <div
-            style={{
-              padding: "50px 300px",
-              color: "red",
-            }}
-          >
-            <p style={{ fontWeight: "700px" }}>{content.title}</p>
-            <p>{content.desc}</p>
-          </div>
-          <hr style={{ width: "40%" }} />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "20px",
-              padding: "50px 0",
-            }}
-          >
+          {/* <p style={{ fontWeight: "700px" }}>{content.title}</p> */}
+          <p className="text-base py-12 px-[23rem] text-[#5f5f5f] text-left">
+            {content.desc}
+          </p>
+
+          <hr className="text-center w-2/5" />
+
+          <div className="flex justify-center gap-5 pt-12 pb-20">
             <button
               onClick={() => handleFunny()}
+              className="bg-[#2c7edb] text-white "
               style={{
-                backgroundColor: "blue",
-                color: "white",
                 border: "none",
-                padding: "15px 50px",
+                padding: "10px 50px",
+                boxShadow: "0 2px #0c5fbc",
               }}
             >
               This is Funny!
             </button>
             <button
               onClick={() => handleNoFunny()}
+              className="bg-[#29b363] text-white "
               style={{
-                backgroundColor: "green",
-                color: "white",
                 border: "none",
-                padding: "15px 50px",
+                padding: "10px 50px",
+                boxShadow: "0 2px #099042",
               }}
             >
               This is not funny.
@@ -133,16 +151,20 @@ function App() {
 
       {/* Footer */}
       <hr />
-      <div style={{ padding: "50px 300px", color: "gray", fontSize: "14px" }}>
-        <p>
+      <div className="px-80 py-6 text-sm text-gray-500">
+        <p className="m-1">
           This website is created as part of Hlsolutions program. The materials
-          contained on this website are provided for general information only
-          and do not constitute any form of advice. HLS assumes no
-          responsibility for the accuracy of any particular statement and
+          contained on this website are provided for general
+        </p>
+        <p className="m-1">
+          information only and do not constitute any form of advice. HLS assumes
+          no responsibility for the accuracy of any particularstatement and
+        </p>
+        <p className="m-1">
           accepts no liability for any loss or damage which may arise from
           reliance on the information contained on this site.
         </p>
-        <p>Copyright 2021 HLS</p>
+        <p className="text-xs font-medium ">Copyright 2021 HLS</p>
       </div>
       <Toaster />
     </div>
---
+++ src/data.js
@@ -0,0 +1,4 @@
+export const images = {
+  Logo: require("./images/Logo.png"),
+  Avatar: require("./images/avt.png"),
+};
